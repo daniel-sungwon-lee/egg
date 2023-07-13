e52d4cd111a726fff3d11087dc71361d871c54dd
Daniel Sungwon Lee <danlee333@gmail.com>
2023-07-12 19:49:29 -0700

    added egg animation

--- app/egg/page.js
+++ app/egg/page.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Lottie from 'lottie-react'
 import chickAnimation from '/public/lottie/chick.json'
+import { gsap } from 'gsap'
 import { useState } from 'react'
 
 export default function Egg() {
@@ -12,17 +13,28 @@ export default function Egg() {
   return (
     <main className={styles.eggPage}>
       {
-        icon === 'egg' ? <div className={styles.eggImg}>
+        icon === 'egg' ? <div className={`${styles.eggImg} egg`}>
                            <Image src={'/images/egg.svg'} alt='Egg' width={512}
                             height={512} draggable='false' style={{background: '#D8E9E4',
                             cursor: 'pointer'}}
-                            className='image' onClick={() => setIcon('chick')} />
+                            className='image' onClick={() => {
+                              const tl = gsap.timeline()
+                              tl.to('.egg', {transform: 'rotate(15deg)'})
+                                .to('.egg', {transform: 'rotate(-30deg)'})
+                                .to('.egg', {transform: 'rotate(15deg)'})
+                                .to('.egg', {transform: 'rotate(0deg)'})
+                                .to('.egg', {transform: 'rotate(15deg)', duration:0.25}, '<0.5')
+                                .to('.egg', {transform: 'rotate(-30deg)', duration:0.25})
+                                .to('.egg', {transform: 'rotate(15deg)', duration:0.25})
+                                .to('.egg', {transform: 'rotate(0deg)', duration:0.25})
+                                .to('.egg', {opacity: 0, duration: 1, ease: 'power3.inOut',
+                                    onComplete: () => setIcon('chick')}, '<1')
+                            }} />
                          </div>
-                       : <div className={styles.lottie}>
+                       : <div className={`${styles.lottie} chick`} style={{opacity: 1}}>
                            <Lottie animationData={chickAnimation} loop autoplay
-                            draggable='false' className='image' style={{margin: '5rem 0',
-                            cursor: 'pointer', background: '#D8E9E4'}}
-                            onClick={() => setIcon('egg')} />
+                            draggable='false' className='image' style={{margin: '4rem 0',
+                            background: '#D8E9E4'}} />
                          </div>
       }
       <div className={styles.subtitle}>
