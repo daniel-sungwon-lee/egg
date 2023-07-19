09e428b403752573602222fc968e227204df58e3
Daniel Sungwon Lee <danlee333@gmail.com>
2023-07-18 20:05:42 -0700

    added collapse content and font update

--- app/page.js
+++ app/page.js
@@ -2,8 +2,23 @@
 
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Modal } from 'antd/es'
+import localFont from 'next/font/local'
+import { Card, Collapse, Modal } from 'antd/es'
+import { PlusOutlined } from '@ant-design/icons'
 import { useState } from 'react'
+
+const Magda = localFont({ src: '../public/fonts/Magda.otf' })
+
+const items = [
+  {
+    key: '1',
+    label: 'Add your chicken here',
+    children: <Card bordered={false} bodyStyle={{background: 'white'}}
+               className={Magda.className}>
+                Hello
+              </Card>
+  }
+]
 
 export default function Home() {
   const [open, setOpen] = useState(false)
@@ -21,9 +36,14 @@ export default function Home() {
          style={{cursor: 'pointer'}} />
       </div>
 
+      <div className={styles.collapseArea}>
+        <Collapse items={items} ghost size='large' className={Magda.className}
+         expandIcon={({ isActive }) => <PlusOutlined rotate={isActive ? 135 : 0} />} />
+      </div>
+
       <Modal centered open={open} style={{background: 'none'}}
        onCancel={() => setOpen(false)} footer={null} wrapClassName='chickenModal'
-       closeIcon={false}>
+       closeIcon={false} className={Magda.className}>
         <div style={{background: 'white', padding: '1rem'}}>
           This project was inspired by my aunt&apos;s chicken farm that I had to take
           care of during the spring. I remember having to headcount all of the 10
