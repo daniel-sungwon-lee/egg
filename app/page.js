87ff1285d89edd32b7763dd5ea8cac35f77641c1
Daniel Sungwon Lee <danlee333@gmail.com>
2023-07-19 11:03:10 -0700

    data storage and other updates

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import localFont from 'next/font/local'
-import { Button, Card, Collapse, Input, Modal } from 'antd/es'
+import { Button, Card, Collapse, Input, Modal, message } from 'antd/es'
 import { PlusOutlined } from '@ant-design/icons'
 import { useState } from 'react'
 
@@ -17,34 +17,76 @@ export default function Home() {
   const [age, setAge] = useState('')
   const [description, setDescription] = useState('')
 
+  const [messageApi, contextHolder] = message.useMessage();
+  const successMessage = () => {
+    messageApi.open({
+      type: 'success',
+      content: 'Chicken added',
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const idGenerator = () => {
+      const S4 = () => {
+        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+      }
+      return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    }
+    const data = {
+      id: idGenerator(),
+      name,
+      age,
+      description
+    }
+
+    if(localStorage.getItem('chickenHeadcount')) {
+      const existingData = JSON.parse(localStorage.getItem('chickenHeadcount'))
+      const updatedData = [...existingData, data]
+      localStorage.setItem('chickenHeadcount', JSON.stringify(updatedData))
+
+      setName('')
+      setAge('')
+      setDescription('')
+
+      successMessage()
+
+    } else {
+      localStorage.setItem('chickenHeadcount', JSON.stringify([data]))
+
+      setName('')
+      setAge('')
+      setDescription('')
+
+      successMessage()
+    }
   }
 
   const items = [
     {
       key: '1',
       label: 'Add your chicken here',
-      children: <Card bordered={false} bodyStyle={{ background: 'white' }}
-                 className={Magda.className} style={{width: '300px'}}>
-                  <form onSubmit={handleSubmit} style={{background: 'white'}}>
-                    <p style={{background: 'white'}}>Name:</p>
+      children: <Card bordered={false} className={Magda.className}
+                 style={{width: '300px'}}>
+                  <form onSubmit={handleSubmit}>
+                    <p>Name:</p>
                     <Input placeholder='Snowflake' value={name}
                      onChange={(e) => setName(e.target.value)} required
                      className={Magda.className} style={{marginBottom: '1rem'}} />
 
-                    <p style={{background: 'white'}}>Age:</p>
+                    <p>Age:</p>
                     <Input placeholder='3 years' value={age}
                      onChange={(e) => setAge(e.target.value)} required
                      className={Magda.className} style={{marginBottom: '1rem'}} />
 
-                    <p style={{background: 'white'}}>Descripton:</p>
+                    <p>Descripton:</p>
                     <TextArea placeholder='Brown with white spots on head'
                      value={description} className={Magda.className}
                      onChange={(e) => setDescription(e.target.value)}
                      style={{marginBottom: '1rem'}} required />
 
-                    <div style={{display: 'flex', justifyContent: 'center', background: 'white'}}>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
                       <Button htmlType='submit' shape='round' className={Magda.className}
                        style={{background: '#FCE0B0'}} type='text'>
                         Submit
@@ -68,15 +110,21 @@ export default function Home() {
          style={{cursor: 'pointer'}} />
       </div>
 
+      {contextHolder}
       <div className={styles.collapseArea}>
         <Collapse items={items} ghost size='large' className={Magda.className}
-         expandIcon={({ isActive }) => <PlusOutlined rotate={isActive ? 135 : 0} />} />
+         expandIcon={({ isActive }) => <PlusOutlined rotate={isActive ? 135 : 0} />}
+         onChange={() => {
+          setName('')
+          setAge('')
+          setDescription('')
+         }} />
       </div>
 
-      <Modal centered open={open} style={{background: 'none'}}
+      <Modal centered open={open} className={Magda.className}
        onCancel={() => setOpen(false)} footer={null} wrapClassName='chickenModal'
-       closeIcon={false} className={Magda.className}>
-        <div style={{background: 'white', padding: '1rem'}}>
+       closeIcon={false}>
+        <div style={{padding: '1rem'}}>
           This project was inspired by my aunt&apos;s chicken farm that I had to take
           care of during the spring. I remember having to headcount all of the 10
           chickens by the end of the day (to make sure all of them returned to the coop),
