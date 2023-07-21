42658c730b16b1e3605bae1784f96f8bf11f540e
Daniel Sungwon Lee <danlee333@gmail.com>
2023-07-21 10:03:29 -0700

    opengraph update

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,20 @@ const Magda = localFont({src: '../public/fonts/Magda.otf'})
 export const metadata = {
   title: 'Egg',
   description: 'Chicken and Egg tracker',
+  openGraph: {
+    title: 'Egg',
+    description: 'Chicken and Egg tracker',
+    url: 'https://eggggg.netlify.app/',
+    siteName: 'Egg',
+    images: [
+      {
+        url:'/images/egg.svg',
+        width: 512,
+        height: 512
+      }
+    ],
+    type: 'website'
+  }
 }
 
 export default function RootLayout({ children }) {
